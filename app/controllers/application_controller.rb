3a1c82fca875a1b012f4837178df75b908fc4ad5
Albert Callarisa Roca <shark234@gmail.com>
2012-07-28 11:57:36 +0800

    Added simple authentication.
    Also moved root path to petitions index, removing the test url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def test
-    render text: 'Hi', layout: true
+  if ENV["HTTP_USERNAME"] && ENV['HTTP_PASSWORD']
+    http_basic_authenticate_with :name => ENV["HTTP_USERNAME"], :password => ENV['HTTP_PASSWORD']
   end
 end
